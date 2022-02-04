61e17c3f4c9d58492d8555345fd68191e59a8963
Monika Grzanek <monski@o2.pl>
2022-02-04 23:27:52 +0100

    Init files

---
+++ src/components/WeatherBox/WeatherBox.js
@@ -0,0 +1,22 @@
+import PickCity from '../PickCity/PickCity';
+import WeatherSummary from '../WeatherSummary/WeatherSummary';
+import Loader from '../Loader/Loader';
+import { useCallback, useState } from 'react';
+
+
+const WeatherBox = props => {
+
+  const handleCityChange = useCallback(city => {
+    console.log(`city:${city}`);
+  }, []);
+
+  return (
+    <section>
+      <PickCity action={handleCityChange} />
+      <WeatherSummary />
+      <Loader />
+    </section>
+  )
+};
+
+export default WeatherBox;
