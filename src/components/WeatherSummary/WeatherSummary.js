6ff20e3dd7392023dd4a9e44ceeddc01d1571f57
Monika Grzanek <monski@o2.pl>
2022-02-05 18:33:34 +0100

    Add fetch and weatherData in WeatherBox.js and sen to child WeatherSummary

--- src/components/WeatherSummary/WeatherSummary.js
+++ src/components/WeatherSummary/WeatherSummary.js
@@ -1,16 +1,16 @@
 import styles from './WeatherSummary.module.scss';
 
-const WeatherSummary = ({...props}) => {
+const WeatherSummary = props => {
   return (
     <section className={styles.weatherSummary}>
       <img
         className={styles.weatherIcon}
-        alt={props.description}
-        src={`${process.env.PUBLIC_URL}/images/weather-icons/${props.icon}.png`} />
+        alt={props.value.description}
+        src={`${process.env.PUBLIC_URL}/images/weather-icons/${props.value.icon}.png`} />
       <div className={styles.weatherInfo}>
-        <h2>London</h2>
+        <h2>{props.value.city}</h2>
         <p>
-          <strong>Temp:</strong> {props.temp}
+          <strong>Temp:</strong> {props.value.temp}
         </p>
       </div>
     </section>
