c3d2845ea2bceda86729f8699dbc3fccb38e653b
Monika Grzanek <monski@o2.pl>
2022-02-05 23:55:32 +0100

    Add condition to render ErrorBox and correct in WeatherSummary

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -2,25 +2,34 @@ import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 import { useCallback, useState } from 'react';
+import ErrorBox from '../ErrorBox/ErrorBox.js'
 
 
 const WeatherBox = () => {
-  const [weatherData, setWeatherData] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
   const [pending, setPending] = useState(false);
+  const [mistake, setMistake] = useState(false);
 
   const handleCityChange = useCallback(value => {
     setPending(true);
+    setMistake(false);
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${value}&appid=5d9df579968077defb121d007ddd656a&units=metric`)
-      .then(res => res.json())
-      .then(data => {
-        setPending(false);
-        console.log(`city:${value}`, data);
-        const weatherData = {
-          city: data.name,
-          temp: data.main.temp,
-          icon: data.weather[0].icon,
-          description: data.weather[0].main
-        };
+      .then(res => {
+          if(res.status === 200) {
+            return res.json()
+              .then(data => {
+                setPending(false);
+                console.log(`city:${value}`, data);
+                setWeatherData({
+                  city: data.name,
+                  temp: data.main.temp,
+                  icon: data.weather[0].icon,
+                  description: data.weather[0].main
+                })
+              })
+          } else {
+              setMistake(true);
+          }
       });
   }, []);
 
@@ -28,8 +37,9 @@ const WeatherBox = () => {
   return (
     <section>
       <PickCity action={handleCityChange} />
-      {(weatherData && !pending) && <WeatherSummary value={weatherData} onChange={e => setWeatherData(e.target.value)} /> }
-      {pending && <Loader />}
+      {(weatherData && !pending && !mistake) && <WeatherSummary weatherData={weatherData} /> }
+      {(pending && !mistake) && <Loader />}
+      {mistake && <ErrorBox />}
     </section>
   )
 };
--- src/components/WeatherSummary/WeatherSummary.js
+++ src/components/WeatherSummary/WeatherSummary.js
@@ -1,16 +1,16 @@
 import styles from './WeatherSummary.module.scss';
 
-const WeatherSummary = props => {
+const WeatherSummary = ({weatherData}) => {
   return (
     <section className={styles.weatherSummary}>
       <img
         className={styles.weatherIcon}
-        alt={props.value.description}
-        src={`${process.env.PUBLIC_URL}/images/weather-icons/${props.value.icon}.png`} />
+        alt={weatherData.description}
+        src={`${process.env.PUBLIC_URL}/images/weather-icons/${weatherData.icon}.png`} />
       <div className={styles.weatherInfo}>
-        <h2>{props.value.city}</h2>
+        <h2>{weatherData.city}</h2>
         <p>
-          <strong>Temp:</strong> {props.value.temp}
+          <strong>Temp:</strong> {weatherData.temp} °C
         </p>
       </div>
     </section>
